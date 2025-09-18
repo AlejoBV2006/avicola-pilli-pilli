ca2a6c539530f87b7ad074ce4eb41c15af6e91c7
AlejoBV2006 <140457143+AlejoBV2006@users.noreply.github.com>
2025-09-17 20:25:35 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css"; // Puedes mover los estilos ahí si prefieres
+import "./App.css"; 
 
 export default function App() {
   const [activeTab, setActiveTab] = useState("productos");
